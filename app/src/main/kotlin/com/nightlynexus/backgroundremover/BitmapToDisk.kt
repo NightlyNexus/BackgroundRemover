9c6e1e868dcdc399514c9ee185d37aa91856cfcf
Eric Cochran <Eric@NightlyNexus.com>
2025-02-23 07:23:50 -0500

    Fix crashing when trying to save duplicate file.
    It was crashing when I already had 31 of the same named file ("file.png" to "file (31).png").
    
    We now catch the exception Android throws and try naming the file manually. We don't preemptively check for this case because it's so rare, so catching the exception is good enough.

--- app/src/main/kotlin/com/nightlynexus/backgroundremover/BitmapToDisk.kt
+++ app/src/main/kotlin/com/nightlynexus/backgroundremover/BitmapToDisk.kt
@@ -3,6 +3,7 @@ package com.nightlynexus.backgroundremover
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
 import android.provider.MediaStore.Images.Media.DISPLAY_NAME
@@ -19,16 +20,18 @@ internal sealed interface SaveBitmapResult {
   object Failure : SaveBitmapResult
 }
 
+private const val directoryName = "BackgroundRemover"
+
 @WorkerThread internal fun saveBitmapAsPng(
   contentResolver: ContentResolver,
   bitmap: Bitmap,
   fileName: String
 ): SaveBitmapResult {
+  // Avoid .jpg.png resulting names.
+  val fileNameWithoutExtension = fileName.substringBeforeLast(".")
   val values = ContentValues().apply {
-    // Avoid .jpg.png resulting names.
-    val fileNameWithoutExtension = fileName.substringBeforeLast(".")
     put(DISPLAY_NAME, fileNameWithoutExtension)
-    put(RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}${File.separator}BackgroundRemover")
+    put(RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}${File.separator}$directoryName")
     put(MediaStore.Images.Media.MIME_TYPE, "image/png")
     put(MediaStore.Images.Media.IS_PENDING, 1)
   }
@@ -53,9 +56,76 @@ internal sealed interface SaveBitmapResult {
   // We clear because we are only updating the IS_PENDING flag.
   values.clear()
   values.put(MediaStore.Images.Media.IS_PENDING, 0)
-  contentResolver.update(outputUri, values, null, null)
+  val updated = try {
+    contentResolver.rigorousUpdate(outputUri, values, fileName, fileNameWithoutExtension)
+  } catch (t: Throwable) {
+    contentResolver.delete(outputUri, null, null)
+    throw t
+  }
+  if (!updated) {
+    contentResolver.delete(outputUri, null, null)
+    return SaveBitmapResult.Failure
+  }
   // We can't simply use the file path we give the ContentResolver because duplicate names can
   // result in a changed path (such as "my-image (2).png").
   val resultingFilePath = filePath(contentResolver, outputUri)
   return SaveBitmapResult.Success(resultingFilePath)
+}
+
+// Returns true if and only if ContentResolver.update runs.
+// Returns false if and only if there are too many duplicate files to find a name for the file.
+private fun ContentResolver.rigorousUpdate(
+  uri: Uri,
+  values: ContentValues,
+  fileName: String,
+  fileNameWithoutExtension: String
+): Boolean {
+  var error: Throwable
+  try {
+    update(uri, values, null, null)
+    // The first try is a success. Get out of here.
+    return true
+  } catch (t: Throwable) {
+    error = t
+    // Fall through to the error if this is not an IllegalStateException.
+    if (t is IllegalStateException) {
+      val message = t.message
+      // Fall through to the error if this is not the right kind of IllegalStateException.
+      if (message != null && message.startsWith("Failed to build unique file")) {
+        val directory = File(
+          Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+          directoryName
+        )
+        val baseFile = File(directory, "$fileNameWithoutExtension.png")
+        // Fall through to the error if the base file does not exist because the ContentResolver
+        // should have been able to save the base file.
+        if (baseFile.exists()) {
+          var count = 2
+          while (true) {
+            val newFileNameWithoutExtension = "$fileNameWithoutExtension ($count)"
+            val countFile = File(directory, "$newFileNameWithoutExtension.png")
+            if (!countFile.exists()) {
+              // We found an available file name. Let's try again.
+              values.put(DISPLAY_NAME, newFileNameWithoutExtension)
+              try {
+                update(uri, values, null, null)
+              } catch (tFromSecondTry: Throwable) {
+                error = tFromSecondTry
+                // That still did not work. Fall through to the error.
+                break
+              }
+              // The fallback is a success. Get out of here.
+              return true
+            }
+            if (count == Integer.MAX_VALUE) {
+              // That is a lot of files. Give up. Get out of here.
+              return false
+            }
+            count++
+          }
+        }
+      }
+    }
+  }
+  throw IllegalStateException("Error saving file: $fileName", error)
 }
